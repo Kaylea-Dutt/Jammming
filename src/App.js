789113973cb59b4be4656b250b224f7434b5f7e8
Kaylea Dutt <kaydutt1138@gmail.com>
2024-06-21 16:52:54 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
 
 function App() {
+
+const CLIENT_ID = "5185c6db0d36414d892de9fe5afa1dba";
+const REDIRECT_URI = "http://localhost:3000";
+const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const RESPONSE_TYPE = "token";
+
   return (
     <div className="App">
-      <h1>Testing</h1>
+      <h1>Spotify App</h1>
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>login to spotify</a>
     </div>
   );
 }
