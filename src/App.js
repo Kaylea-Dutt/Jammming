b798fe9b6c474769a7c2d4c169cbbfb7d24c12c9
Kaylea Dutt <kaydutt1138@gmail.com>
2024-06-24 16:38:29 -0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
 
@@ -7,10 +9,75 @@ const REDIRECT_URI = "http://localhost:3000";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
 const RESPONSE_TYPE = "token";
 
+const [token, setToken] = useState('');
+const [searchKey, setSearchKey] = useState('');
+const [artists, setArtists] = useState([]);
+
+useEffect(() => {
+const hash = window.location.hash
+let token = window.localStorage.getItem("token")
+
+if(!token && hash) {
+  token = hash.substring(1).split("&").find(elem => elem.startsWith("access_token")).split("=")[1]
+
+  window.location.hash = "";
+  window.localStorage.setItem("token", token);
+
+}
+
+setToken(token);
+
+}, []);
+
+const logout = () => {
+  setToken("");
+  window.localStorage.removeItem("token");
+}
+
+const searchArtists = async (e) => {
+  e.preventDefault();
+  const {data} = await axios.get("https://api.spotify.com/v1/search", {
+    headers: {
+      Authorization: `Bearer ${token}`
+    },
+      params: {
+        q: searchKey,
+        type: "artist"
+      }
+})
+
+setArtists(data.artists.items)
+}
+
+const RenderArtists = () => {
+  return artists.map(artist => (
+    <div key={artist.id}>
+      {artist.images.length ? <img width={"50%"} src={artist.images[0].url} alt="" /> : <div>No Image</div>}
+      {artist.name}
+
+    </div>
+  ))
+}
+
   return (
     <div className="App">
-      <h1>Spotify App</h1>
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>login to spotify</a>
+      <header className="App-header">
+      <h1>Jammming</h1>
+      
+      {!token ? <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>login to spotify</a>
+      : <button onClick={logout}>Logout</button>}
+
+  {token ?
+    <form onSubmit={searchArtists}>
+      <input type= "text" onChange={e => setSearchKey(e.target.value)} />
+      <button type="submit">Search</button>
+    </form>  
+      : <h2>Please login</h2>
+  }
+
+  <RenderArtists />
+
+    </header>
     </div>
   );
 }
